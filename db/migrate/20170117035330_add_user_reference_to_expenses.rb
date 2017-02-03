93f3bc8622012dc5a6f36d7ce9b6b1325982ea33
Ben Jervis <ben@jervis.net.au>
2017-02-03 11:33:25 +0000

    Updated migration to remove reference type causing error in heroku

--- db/migrate/20170117035330_add_user_reference_to_expenses.rb
+++ db/migrate/20170117035330_add_user_reference_to_expenses.rb
@@ -1,5 +1,5 @@
 class AddUserReferenceToExpenses < ActiveRecord::Migration[5.0]
   def change
-    add_column :expenses, :user_id, :reference
+    add_column :expenses, :user_id, :integer
   end
 end
